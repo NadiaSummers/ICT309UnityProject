fdc7fc1f09ae45ef4e2044cac9b2c8140bad2922
Nadia Summers <jazzalude@outlook.com>
2014-05-21 22:55:41 +0800

    Initial Upload
    Basic character control/camera & environment.

---
+++ Assets/Standard Assets/CameraLook.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraLook : MonoBehaviour {
+
+	private float mouseX = 0.0f;
+	public float mouseSensitivity = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mouseX = Input.GetAxis ("Mouse X");
+
+		Vector3 rotation = transform.localEulerAngles;
+		rotation.y += mouseX * mouseSensitivity;
+		transform.localEulerAngles = rotation;
+	}
+}
---
+++ Assets/Standard Assets/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	CharacterController controller;
+
+	public float moveSpeed = 20.0f;
+	public float jumpSpeed = 10.0f;
+	public float gravity = 0.7f;
+	private float yVelocity = 0.0f;
+
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController> ();
+		Screen.lockCursor = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Vector3 direction = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+		Vector3 velocity = direction * moveSpeed;
+
+
+
+		if (controller.isGrounded) {
+			if (Input.GetButtonDown ("Jump")) {
+				yVelocity = jumpSpeed;
+			}
+		}
+		else {
+			yVelocity -= gravity;
+		}
+
+		velocity.y = yVelocity;
+
+
+		velocity = transform.TransformDirection (velocity);
+		controller.Move (velocity * Time.deltaTime);
+	}
+}
