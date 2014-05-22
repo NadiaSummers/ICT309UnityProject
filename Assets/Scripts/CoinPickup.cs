7d06bd3b0bac5869c18caa3d25363348dfade710
Nadia Summers <jazzalude@outlook.com>
2014-05-23 00:53:26 +0800

    Added Cage & Coin Prefabs

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinPickup : MonoBehaviour {
+
+	public int coinValue = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//TODO
+		//check for collision w/ player
+			//add to score
+			//remove object from world
+	}
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	public int rotateSpeed = 100;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3(0, 0, rotateSpeed) * Time.deltaTime);
+	}
+}
