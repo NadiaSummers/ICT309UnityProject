55f9a472fef85685e6f2e9594db61454ec96ef46
Nadia Summers <jazzalude@outlook.com>
2014-05-23 03:59:11 +0800

    Added Troll Prefab & Basic Behaviours

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+	public int maxHealth = 100;
+	private int currentHealth = 0;
+
+	// Use this for initialization
+	void Start () {
+		currentHealth = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Damage(int damageValue)
+	{
+		currentHealth -= damageValue;
+
+		if (currentHealth <= 0)
+		{
+			currentHealth = 0;
+		}
+	}
+
+	public bool IsDead { get{ return currentHealth <= 0; } }
+
+	override public string ToString()
+	{
+		return currentHealth + "/" + maxHealth;
+	}
+}
