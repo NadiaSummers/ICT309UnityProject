d6ec7b0eb01c9987cd4f1ab7f8045611aa78c372
Nadia Summers <jazzalude@outlook.com>
2014-05-25 18:02:15 +0800

    Added CauseDamage script, updated Tags and Troll

---
+++ Assets/Scripts/CauseDamage.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CauseDamage : MonoBehaviour {
+
+	public int damageTaken;
+	public bool singleUse;
+	public string[] tags;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		for (int i = 0; i < tags.Length; i++)
+		{
+			if (other.tag == tags[i])
+			{
+				if (singleUse)
+					Destroy (this.gameObject);
+				
+				Health thisGuy = other.GetComponent<Health>();
+				thisGuy.Damage(damageTaken);
+			}
+		}
+	}
+}
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,25 +5,45 @@ public class Health : MonoBehaviour {
 
 	public int maxHealth = 100;
 	private int currentHealth = 0;
+	private int lastHealth = 0;
+	private bool hasTakenDamage = false;
 
 	// Use this for initialization
 	void Start () {
 		currentHealth = maxHealth;
+		lastHealth = maxHealth;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 
 	public void Damage(int damageValue)
 	{
+		hasTakenDamage = true;
 		currentHealth -= damageValue;
 
 		if (currentHealth <= 0)
 		{
 			currentHealth = 0;
 		}
+	}
+
+	public int GetHealth()
+	{
+		return currentHealth;
+	}
+
+	public bool HasTakenDamage()
+	{
+		if (hasTakenDamage) {
+						hasTakenDamage = false;
+						return true;
+				} else {
+			return false;
+				}
 	}
 
 	public bool IsDead { get{ return currentHealth <= 0; } }
