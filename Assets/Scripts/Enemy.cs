a2156e922bacd0d04286f2bc84683c6a8fb28d5b
Nadia Summers <jazzalude@outlook.com>
2014-05-25 14:04:38 +0800

    Fixed Troll Chasing Behaviour & Animation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -12,7 +12,7 @@ public class Enemy : MonoBehaviour
 		public float gravity = 0.7f;
 		private float yVelocity = 0.0f;
 
-		public float sightRange = 40.0f;
+		public float sightRange = 80.0f;
 		public float attackRange = 5.0f;
 		public int attackDamage = 20;
 
@@ -20,7 +20,10 @@ public class Enemy : MonoBehaviour
 		private float attackDelay = -1.0f;
 		private bool targetInRange = false;
 
-		Animation animation;
+		private new Animation animation;
+
+		private Vector3 velocity;
+		private Vector3 direction;
 
 		// Use this for initialization
 		void Start ()
@@ -28,7 +31,6 @@ public class Enemy : MonoBehaviour
 	
 				GameObject playerGameObject = GameObject.FindGameObjectWithTag ("Player");
 				player = playerGameObject.transform;
-
 
 				controller = GetComponent<CharacterController> ();
 				initialPosition = transform.position;
@@ -46,64 +48,70 @@ public class Enemy : MonoBehaviour
 		{
 	
 				float distance = Vector3.Distance (player.position, transform.position);
-
-				Vector3 direction = player.position - transform.position;
-				direction.Normalize ();
-		
-				Vector3 velocity = direction * moveSpeed;
-		
-
-
 				float homeDistance = Vector3.Distance (initialPosition, transform.position);
+				float homeToPlayerDistance = Vector3.Distance (initialPosition, player.position);
+
 
 				//if we're at our spawn
-				if (distance >= sightRange && homeDistance <= 2) {
+				if (homeToPlayerDistance > sightRange && homeDistance <= 2) {
 						animation.Play ("Idle_01");
 						targetInRange = false;
 						doGravity();
+						//Debug.Log("Troll At Home");
 				}
-				//if we should stop chasing and return home (because player has outpaced us or we're too far from home
-				else if (distance >= sightRange && homeDistance > 2 || homeDistance > (sightRange * 1.5)){
-						animation.Play ("Run");
+				//if we should stop chasing and return home (because we're too far from home)
+				else if (homeToPlayerDistance > sightRange)
+				{
 						targetInRange = false;
 						direction = initialPosition - transform.position;
 						direction.Normalize ();
 						velocity = direction * moveSpeed;
-						transform.rotation = Quaternion.LookRotation (direction);
 						doGravity();
+						transform.rotation = Quaternion.LookRotation (direction);
 						controller.Move (velocity * Time.deltaTime);
+						animation.Play ("Run");
+						//Debug.Log("Troll Going Home :(");
 
-		}
-				else {
-						if (distance <= attackRange)
-							targetInRange = true;
-						else
-							targetInRange = false;
-
+				}
+				else if (homeToPlayerDistance < sightRange)
+				{
+					if (distance <= attackRange)
+						targetInRange = true;
+					else
+						targetInRange = false;
+						
 						//gogo combat
 						if (!targetInRange)
 						{
+								direction = player.position - transform.position;
+								direction.Normalize ();
 								transform.rotation = Quaternion.LookRotation (direction);
-								doGravity();
-								controller.Move (velocity * Time.deltaTime);
-								animation.Play ("Run");
+								if (!animation.IsPlaying ("Attack_01"))
+								{
+									velocity = direction * moveSpeed;
+									doGravity();
+									transform.rotation = Quaternion.LookRotation (direction);
+									controller.Move (velocity * Time.deltaTime);
+									animation.Play ("Run");
+									//Debug.Log("Troll Chasing!");
+								}
 						} 
 						else
 			         	{
 								if (Time.time >= attackDelay)
 								{
 									animation.Play ("Attack_01");
-									yield WaitForSeconds (animation["Attack_01"].length);
-									//animation.PlayQueued ("Idle_01", QueueMode.CompleteOthers);
+									animation.PlayQueued ("Run", QueueMode.CompleteOthers);
 									Health playerHealth = player.GetComponent<Health> ();
 									playerHealth.Damage (attackDamage);
 									attackDelay = Time.time + attackSpeed;
+									//Debug.Log("Troll Smash!");
 									
 								}
 						}
-
 					}
-	}
+					}
+
 	
 	private void doGravity()
 	{
