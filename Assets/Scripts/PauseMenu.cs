8071185420d703734bcedaa79ccb1eeb52284d56
Nadia Summers <jazzalude@outlook.com>
2014-05-26 02:41:51 +0800

    Added Music, Frontend and Pause UI

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+	public Texture2D resumeGameButton;
+	public Texture2D quitGameButton;
+	public GUISkin guiSkin;
+	private bool isPaused = false;
+	
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("escape")) {
+			isPaused = true;
+			Screen.lockCursor = false;
+		}
+	}
+	
+	void OnGUI()
+	{
+		if (isPaused) {
+			Time.timeScale = 0;
+						GUI.skin = guiSkin;
+						if (GUI.Button (new Rect (20, Screen.height - 200, 200, 61), resumeGameButton)) {
+							isPaused = false;
+							Time.timeScale = 1;
+							Screen.lockCursor = true;
+						}
+		
+						if (GUI.Button (new Rect (20, Screen.height - 100, 200, 61), quitGameButton)) {
+								Application.Quit ();
+						}
+		}
+	}
+}
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Singleton : MonoBehaviour {
+
+	private static Singleton instance;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public static Singleton GetInstance(){
+		return instance;
+	}
+	
+	void Awake() {
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
